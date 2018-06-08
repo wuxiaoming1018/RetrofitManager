e5ecb581bdb203074fbfa8a47a5559c567c48a29
Administrator <1398565099@qq.com>
2018-06-08 19:07:10 +0800

    下载文件相关操作

--- app/src/main/java/com/android/ming/retrofitmanager/net/download/DownLoadHandler.java
+++ app/src/main/java/com/android/ming/retrofitmanager/net/download/DownLoadHandler.java
@@ -1,4 +1,75 @@
 package com.android.ming.retrofitmanager.net.download;
 
+import android.os.AsyncTask;
+
+import com.android.ming.retrofitmanager.net.RestCreeator;
+import com.android.ming.retrofitmanager.net.callback.IError;
+import com.android.ming.retrofitmanager.net.callback.IFailure;
+import com.android.ming.retrofitmanager.net.callback.IRequest;
+import com.android.ming.retrofitmanager.net.callback.ISuccess;
+
+import java.util.Map;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+
 public class DownLoadHandler {
+    private final Map<String, Object> PARAMS; //地址参数
+    private final String URL;
+    private final IRequest REQUEST;
+    private final ISuccess SUCCESS;
+    private final IFailure FAILURE;
+    private final IError ERROR;
+    private final String DOWNLOAD_DIR; //下载文件夹
+    private final String EXTENSION; //扩展名
+    private final String FILENAME; //文件名
+
+
+    public DownLoadHandler(Map<String, Object> params, String url,
+                           IRequest request, ISuccess success,
+                           IFailure failure, IError error,
+                           String download_dir, String extension,
+                           String filename) {
+        PARAMS = params;
+        URL = url;
+        REQUEST = request;
+        SUCCESS = success;
+        FAILURE = failure;
+        ERROR = error;
+        DOWNLOAD_DIR = download_dir;
+        EXTENSION = extension;
+        FILENAME = filename;
+    }
+
+    public final void handleDownload(){
+        RestCreeator.getRestService().download(URL,PARAMS)
+                .enqueue(new retrofit2.Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if (response.isSuccessful()) {
+                            SaveFileTask task = new SaveFileTask(REQUEST, SUCCESS);
+                            task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,
+                                    DOWNLOAD_DIR,EXTENSION,response.body(),FILENAME);
+                            //如果下载完成
+                            if (task.isCancelled()) {
+                                if (REQUEST != null) {
+                                    REQUEST.onRequestEnd();
+                                }
+                            }
+                        }else{
+                            if (ERROR != null) {
+                                ERROR.onError(response.code(),response.message());
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        if (FAILURE != null) {
+                            FAILURE.onFailure();
+                        }
+                    }
+                });
+    }
 }
--- app/src/main/java/com/android/ming/retrofitmanager/net/download/SaveFileTask.java
+++ app/src/main/java/com/android/ming/retrofitmanager/net/download/SaveFileTask.java
@@ -1,4 +1,72 @@
 package com.android.ming.retrofitmanager.net.download;
 
-public class SaveFileTask {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import com.android.ming.retrofitmanager.config.ProjectInit;
+import com.android.ming.retrofitmanager.net.callback.IRequest;
+import com.android.ming.retrofitmanager.net.callback.ISuccess;
+import com.android.ming.retrofitmanager.util.FileUtil;
+
+import java.io.File;
+import java.io.InputStream;
+
+import okhttp3.ResponseBody;
+
+public class SaveFileTask extends AsyncTask<Object,Void,File>{
+
+    private final IRequest REQUEST;
+    private final ISuccess SUCCESS;
+
+    public SaveFileTask(IRequest request, ISuccess success) {
+        REQUEST = request;
+        SUCCESS = success;
+    }
+
+    @Override
+    protected File doInBackground(Object... params) {
+        String downloadDir = (String) params[0];
+        String extension=(String)params[1];
+        ResponseBody body=(ResponseBody)params[2];
+        String name=(String)params[3];
+        InputStream inputStream = body.byteStream();
+        if (downloadDir == null||downloadDir.equals("")) {
+            downloadDir = "down_loads";
+        }
+
+        if (extension == null) {
+            extension = "";
+        }
+        if (name == null) {
+            return FileUtil.writeToDisk(inputStream,downloadDir,extension.toUpperCase(),extension);
+        }else{
+            return FileUtil.writeToDisk(inputStream,downloadDir,name);
+        }
+    }
+
+    //如果文件已经下载完毕
+
+    @Override
+    protected void onPostExecute(File file) {
+        super.onPostExecute(file);
+        if (SUCCESS != null) {
+            SUCCESS.onSuccess(file.getPath());
+        }
+        if (REQUEST != null) {
+            REQUEST.onRequestEnd();
+        }
+        //如果下载的文件是apk，直接安装
+        autoInstallApk(file);
+    }
+
+    private void autoInstallApk(File file) {
+        if (FileUtil.getExtension(file.getPath()).equals("apk")) {
+            final Intent intent = new Intent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setDataAndType(Uri.fromFile(file),"application/vnd.android.package-archive");
+            ProjectInit.getApplicationContext().startActivity(intent);
+        }
+    }
 }
